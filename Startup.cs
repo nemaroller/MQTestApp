13b78fca2c62a0e1d2b66459caa88e7439917d56
Karl Beyer <kbeyer@nrtwebservices.com>
2021-06-14 14:05:20 -0500

    updated

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace MQTestApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,14 +37,12 @@ namespace MQTestApp
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
